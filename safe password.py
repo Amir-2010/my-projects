8a348c38cb0194ca1c4ae2b8b14295a835219aa6
Amir-2010 <Amir.hozhabri.rk2025@gmail.com>
2025-05-30 20:07:49 +0330

    Add files via upload

---
+++ safe password.py
@@ -0,0 +1,16 @@
+
+import string
+import random
+
+def security(length):
+    caracters=string.digits + string.ascii_letters + string.punctuation
+    security_password="".join(random.choice(caracters) for i in range(length))
+    # you can write line number 7 like here
+    # security = ""
+    # for i in range(length):
+    #     password=random.choice(caracters)
+    #     security_password+=password
+    return security_password
+
+our_password=security(10)
+print(our_password)
